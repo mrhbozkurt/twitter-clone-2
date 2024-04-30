635ae314c79e1699c86502c55b69f867728f2472
mrhbozkurt <mrhbozkurt@gmail.com>
2024-05-01 01:26:01 +0300

    Modal, Button and Input

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import Modal from "@/components/Modal";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
