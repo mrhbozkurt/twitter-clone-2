eeda84fc1785261e1786d8d0512c7c3d61e0f9cb
mrhbozkurt <mrhbozkurt@gmail.com>
2024-04-30 21:21:58 +0300

    first upload

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+import { BiArrowBack } from "react-icons/bi";
+
+interface HeaderProps {
+    label: string;
+    showBackArrow?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({label, showBackArrow}) => {
+    const router = useRouter();
+    const handleBack = useCallback(() => {
+        router.back();
+    }, [router]);
+
+    return (
+        <div className="border-b-[1px] border-neutral-800 p-5">
+            <div className="flex flex-row items-center gap-2">
+                {
+                    showBackArrow && (
+                        <BiArrowBack className="cursor-pointer hover:opacity-70 transition"
+                        onClick={handleBack} size={20} color="white" />
+                    )
+                }
+                <h1 className="text-white text-xl font-semibold">{label}</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
