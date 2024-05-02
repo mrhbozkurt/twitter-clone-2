fddc51d298204056597f56992a2970319b0111a0
mrhbozkurt <mrhbozkurt@gmail.com>
2024-05-02 17:05:15 +0300

    Adding prisma and mongodb

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,11 +1,18 @@
 import {useRouter} from 'next/router';
+import { useCallback } from 'react';
 import { FaFeather } from 'react-icons/fa';
+import useLoginModal from '@/hooks/useLoginModal';
 
 const SidebarTweetButton = () => {
     const router = useRouter();
+    const loginModal = useLoginModal();
+
+    const onClick = useCallback(() => {
+        loginModal.onOpen();
+    }, [loginModal]);
 
     return (
-        <div onClick={() => router.push('/')}>
+        <div onClick={onClick}>
             <div className='mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer'>
                 <FaFeather size={24} color="white" />
             </div>
