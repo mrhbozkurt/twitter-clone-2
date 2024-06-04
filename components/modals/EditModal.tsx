7d2ca23d6df9c9b0cc8d2a49b3e4e226fbfe3259
mrhbozkurt <mrhbozkurt@gmail.com>
2024-06-05 00:16:50 +0300

    Profile Image and Cover Upload

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -6,6 +6,7 @@ import useEditModal from "@/hooks/useEditModal";
 import useUser from "@/hooks/useUser";
 import Modal from "../Modal";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 const EditModal = () => {
     const { data: currentUser } = useCurrentUser();
@@ -31,7 +32,7 @@ const EditModal = () => {
     const onSubmit = useCallback(async () => {
         try {
             setIsLoading(true);
-
+            
             await axios.patch('/api/edit', {
                 name,
                 username,
@@ -44,8 +45,12 @@ const EditModal = () => {
             toast.success('Updated');
 
             editModal.onClose();
+
         } catch(error) {
+
+            console.log(error);
             toast.error('Something went wrong');
+
         } finally {
             setIsLoading(false);
         }
@@ -53,6 +58,18 @@ const EditModal = () => {
 
     const bodyContent = (
         <div className=" flex flex-col gap-4">
+            <ImageUpload 
+                value={profileImage}
+                disabled={isLoading}
+                onChange={(image) => setProfileImage(image)}
+                label="Upload profile image"
+            />
+            <ImageUpload 
+                value={coverImage}
+                disabled={isLoading}
+                onChange={(image) => setCoverImage(image)}
+                label="Upload cover image"
+            />
             <Input 
                 placeholder="Name"
                 onChange={(e) => setName(e.target.value)}
